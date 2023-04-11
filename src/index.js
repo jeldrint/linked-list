5bfdaf0d5ca654227f50578b75d1534c32501b08
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-04-12 03:09:04 +0800

    all done. even the Extra Credit (100% working)

--- src/index.js
+++ src/index.js
@@ -54,7 +54,75 @@ const LinkedList = (node) => {
         popNode.nextNode = null;
     }
 
-    return {head, size, tail, append, prepend, pop, at};
+    const contains = (value) => {
+        let containsNode = node;
+        let nodeSize = size();
+
+        for (let i = 1; i <= nodeSize; i++){
+            if(containsNode.value === value){
+                return true;
+            }
+            containsNode = containsNode.nextNode;
+        }
+        return false;
+    }
+
+    const find = (value) => {
+        let findNode = node;
+        let nodeSize = size();
+
+        for (let index = 1; index <= nodeSize; index++){
+            if(findNode.value === value){
+                return `The value is found on index ${index}`;
+            }
+            findNode = findNode.nextNode;
+
+        }
+        return `The value cannot be found. ${null}`;
+    }
+
+    const insertAt = (value,index) => {
+        let insertAtNode = node;
+        if(index == 1){
+            value.nextNode = insertAtNode.nextNode
+            insertAtNode.nextNode = value;
+        }else{
+            for (let i =1; i < index; i++){
+                insertAtNode = insertAtNode.nextNode;
+            }
+            if(insertAtNode == null || index == 0){
+                console.log('Index does not match the current size')
+            }else{
+                value.nextNode = insertAtNode.nextNode
+                insertAtNode.nextNode = value;        
+            }
+        }
+    }
+
+    const removeAt = (index) => {
+        let currentNode = node;
+        let prevNode;
+
+        if(index == 1){
+            prevNode = currentNode.nextNode; // this is the new Head;
+            node = prevNode;                 // transferred to original node;
+
+            currentNode.nextNode = null;
+        }else{
+            for (let i =1; i<index; i++){
+                prevNode = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+            if(currentNode == null || index == 0){
+                console.log('Index does not match the current size')
+            }else{
+                prevNode.nextNode = currentNode.nextNode;
+                currentNode.nextNode = null;        
+            }
+        }
+    }
+
+    return {head, size, tail, append, prepend, pop, at, contains, find, insertAt, removeAt};
 }
 
 const Node = (value) => {
@@ -63,13 +131,34 @@ const Node = (value) => {
 }
 
 const newNode1 = Node('O');
-const newNode2 = Node('H');
-const newNode3 = Node('C');
-const newNode4 = Node('E');
 
-newNode1.nextNode =newNode2;
-newNode2.nextNode =newNode3;
-newNode3.nextNode =newNode4;
-
+//APPEND TESTING
 const list = LinkedList(newNode1);
+list.append(Node('Hello'))
+list.append(Node('Try'))
 
+//PREPEND TESTING
+const newNode2 = Node('Single');
+list.prepend(newNode2)
+
+//POP TESTING
+list.pop();
+
+//AT TESTING
+//console.log(list.at(2));
+
+//CONTAINS TESTING
+//console.log(list.contains('Try'));
+
+//FIND TESTING
+//console.log(list.find('Try'))
+
+//INSERT-AT TESTING
+list.insertAt(Node('really?',1))
+list.insertAt(Node('Pogi'),4)
+
+//REMOVE-AT TESTING
+list.removeAt(1)
+
+//MAIN OUTPUT OF LINKED LIST - HEAD
+console.log(list.head());
